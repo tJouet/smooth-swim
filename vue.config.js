5a2947ef28440fb96f57a360e9a67cdd880507d0
Jouet <thibaut.jouet64@gmail.com>
2024-01-18 10:21:00 +0100

    Clean obsolete code

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  rules: [
-    {
-     test: /\.svg$/,
-     loader: 'vue-svg-loader', // `vue-svg` for webpack 1.x
-    },
-  ],
 });
